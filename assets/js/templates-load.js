c5231895576b4ebc36d1d30ef27fae63c9ce1968
Tanish Baidya <tanish.baidya@gmail.com>
2021-03-24 13:23:50 +0530

    Modified templates-load.js

--- assets/js/templates-load.js
+++ assets/js/templates-load.js
@@ -2,7 +2,7 @@ $(document).ready(
   $(function () {
     var includes = $("[data-include]");
     $.each(includes, function () {
-      var file = "/templates/includes/" + $(this).data("include") + ".html";
+      var file = "templates/includes/" + $(this).data("include") + ".html";
       $(this).load(file);
     });
   })
