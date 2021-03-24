5fef63b83d7bedb2072427ba9d107a0a9c1fd04c
Tanish Baidya <tanish.baidya@gmail.com>
2021-03-24 13:08:38 +0530

    Initial Commit

---
+++ assets/js/templates-load.js
@@ -0,0 +1,9 @@
+$(document).ready(
+  $(function () {
+    var includes = $("[data-include]");
+    $.each(includes, function () {
+      var file = "/templates/includes/" + $(this).data("include") + ".html";
+      $(this).load(file);
+    });
+  })
+);
